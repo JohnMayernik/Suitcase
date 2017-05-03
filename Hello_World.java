756fbc0972719a23f9b28d6b73875c845daf7c04
JohnMayernik <jbflowwater@gmail.com>
2017-05-02 21:47:04 -0700

    added .gitignore file

---
+++ Hello_World.java
@@ -0,0 +1,12 @@
+package Hello_World;
+
+/**
+ * Print Hello World to the screen when run.
+ * @author John Mayernik
+ * @version 5/2/2017
+ */
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
