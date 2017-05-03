04955db4cdb170b01489832a082461c933fe0f98
John Mayernik <john.mayernik15@pcc.edu>
2017-05-02 22:20:57 -0700

    added new comment for print statement

--- Hello_World/src/Hello_World/Hello_World.java
+++ Hello_World/src/Hello_World/Hello_World.java
@@ -4,9 +4,17 @@ package Hello_World;
  * Print Hello World to the screen when run.
  * @author John Mayernik
  * @version 5/2/2017
+ *
+ * Changes Made:
+ *  added .gitignore file
+ *  Revised print statement
+ *
  */
 public class Hello_World {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!!" + " \nHow is your day going?");
+
+
+
     }
 }
